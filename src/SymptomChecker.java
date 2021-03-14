009e72d6531cc4f51aaf6e8ebcfb95da33cf4798
mattcmerritt <mattcmerritt@gmail.com>
2021-03-14 12:36:31 -0400

    allowed user to load a calendar or create a new one

--- src/SymptomChecker.java
+++ src/SymptomChecker.java
@@ -11,8 +11,37 @@ import java.util.Scanner;
 public class SymptomChecker {
 
 	public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
 
 		System.out.println("**************Welcome to the Quinnipiac Symptom Checker App****************** \n");
+		
+		System.out.println("Would you like to start a new calendar or load a calendar from a file? (\"load\" to use a file, anything else to start a new one)");
+		String calendarChoice = input.nextLine();
+		Calendar calendar;
+		if (calendarChoice.toLowerCase().equals("load")) {
+			do {
+				File currentDirectory = new File(".");
+				System.out.println("Please input a filename: ");
+				System.out.println("The current directory is " + currentDirectory.getAbsolutePath());
+				String calendarFilename = input.nextLine();
+				calendar = Calendar.loadCalendar(calendarFilename);
+				if (calendar == null) {
+					System.out.println("Sorry, that file could not be found. Please try again.");
+				}
+			} while (calendar == null);
+		}
+		else {
+			calendar = new Calendar();
+			calendar.scheduleUpcomingTests();
+		}
+		
+		System.out.println(calendar.displayCalendar());
+		System.out.println();
+		
+		
+		
+		
 
 		System.out.println("                   Please type in one number for one of the following options:");
 		System.out.println("                   1. SymtomApp (To take your daily Symptom Checker Test)");
@@ -20,7 +49,7 @@ public class SymptomChecker {
 		System.out.println("                   3. UpcomingTest (To view your upcoming test)");
 		System.out.println("                   4. Calender (To view the current month)");
 
-		Scanner input = new Scanner(System.in);
+		
 		Double amount = input.nextDouble();
 		int amountInt = (int) Math.round(amount);
 
