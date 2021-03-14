8bbb1bebb4fdc32364d72a44084aa96bc0d14743
mattcmerritt <mattcmerritt@gmail.com>
2021-03-14 15:45:03 -0400

    removed unused import statements

--- src/SymptomChecker.java
+++ src/SymptomChecker.java
@@ -1,11 +1,4 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class SymptomChecker {
